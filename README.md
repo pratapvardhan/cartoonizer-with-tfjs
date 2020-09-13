6e0094a3ddc84a18fb3bb3e77aa3920f109d7613
Pratap Vardhan <pratap.vardhan@gramener.com>
2020-09-13 11:53:59 -0400

    ENH: Cartoonizer with TensorFlow.js v0

---
+++ README.md
@@ -0,0 +1,55 @@
+# Cartoonizer with TensorFlow.js
+
+[Try application][liveapp]
+
+We used Generative Adversarial Network (GAN) model proposed in 
+[Learning to Cartoonize Using White-box Cartoon Representations][cvpr2020] (CVPR 2020) by Xinrui Wang and Jinze Yu.
+
+Our idea was to test if it is reasonably possible to perform model inferences in
+the browser clients with CPUs only. Without needing to send any of user's data (images) to servers.
+
+**[App preview][liveapp]**:
+
+<TOOD: GIF HERE>
+
+Here's the application flow and architecture:
+
+<p align="center">
+  <img src="./assets/flow.jpg" style="max-width:650px;width:100%;"/>
+</p>
+
+TensorFlow's Saved models are converted to TensorFlow.js models.
+Images are padded and scaled to 256px before they are fed to the model.
+This rescaling is done to speed up the processing and might reduce the quality too.
+
+Model footprint is ~1.5MB. These models in the browsers without GPU acceleration could manage to cartoonize,
+but takes anywhere between 5-10 seconds for processing.
+This is much slower than tflite models performance in mobile devices.
+However, web browsers benefit from users not needing to install anything or transmitting data outside of their systems.
+
+## Credits
+
+This work was possible due to 
+- [Margaret Maynard-Reid](https://twitter.com/margaretmz) and [Sayak Paul](https://twitter.com/RisingSayak)'s work on [How to Create a Cartoonizer with TensorFlow Lite](https://blog.tensorflow.org/2020/09/how-to-create-cartoonizer-with-tf-lite.html)
+- [Xinrui Wang](https://github.com/SystemErrorWang/) and Jinze Yu's original work on [White-box CartoonGAN][cvpr2020]
+
+## Citation
+Xinrui Wang and Jinze Yu are the original authors of [White-box CartoonGAN][cvpr2020].
+```
+@InProceedings{Wang_2020_CVPR,   
+author = {Wang, Xinrui and Yu, Jinze,     
+title = {Learning to Cartoonize Using White-Box Cartoon Representations,   
+booktitle = {IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)},   
+month = {June}, year = {2020} }
+```
+
+### Links
+
+- Original [White-box Cartoonization Repo](https://github.com/SystemErrorWang/White-box-Cartoonization)
+- Cartoonizer with [TensorFlow Lite Repo](https://github.com/margaretmz/Cartoonizer-with-TFLite)
+- [Live application][liveapp]
+- [Repo](https://github.com/pratapvardhan/cartoonizer-with-tfjs/), Notebook, Colab
+
+[cvpr2020]: https://openaccess.thecvf.com/content_CVPR_2020/papers/Wang_Learning_to_Cartoonize_Using_White-Box_Cartoon_Representations_CVPR_2020_paper.pdf
+[liveapp]: https://pratapvardhan.github.io/cartoonizer-with-tfjs/
+[repo]: https://github.com/pratapvardhan/cartoonizer-with-tfjs/
---
+++ script.js
@@ -0,0 +1,92 @@
+tf.setBackend('wasm').then(() => runModel())
+
+const APP = {
+  model: null, size: 256,
+  source: document.getElementById('source'),
+  canvas: document.getElementById('result'),
+  status: document.getElementById('status'),
+  download: document.getElementById('download'),
+  $: n => document.getElementById(n),
+  path: './models/CartoonGAN/web-uint8/model.json'
+}
+
+const runModel = async () => {
+  APP.model = await tf.loadGraphModel(APP.path)
+  // warm up
+  APP.model.predict(tf.zeros([1, 1, 1, 3])).dispose()
+  predict(APP.source)
+  APP.source.onload = () => {
+    setTimeout(() => {
+      APP.status.classList.remove('d-none')
+      APP.canvas.classList.add('d-none')
+      APP.canvas.classList.remove('d-block')
+    }, 0)
+    setTimeout(() => { predict(APP.source) }, 10)
+  }
+}
+
+async function predict(imgElement) {
+  let img = tf.browser.fromPixels(imgElement)
+  const shape = img.shape
+  const [w, h] = shape
+  img = normalize(img)
+  const t0 = performance.now()
+  const result = await APP.model.predict({ 'input_photo:0': img })
+  const timer = performance.now() - t0
+  let img_out = await result.squeeze().sub(tf.scalar(-1)).div(tf.scalar(2)).clipByValue(0, 1)
+  const pad = Math.round(Math.abs(w - h) / Math.max(w, h) * APP.size)
+  const slice = (w > h) ? [0, pad, 0] : [pad, 0, 0]
+  img_out = img_out.slice(slice)
+  draw(img_out, shape)
+  console.log(Math.round(timer / 1000 * 10) / 10)
+}
+
+function normalize(img) {
+  const [w, h] = img.shape
+  // pad
+  const pad = (w > h) ? [[0, 0], [w - h, 0], [0, 0]] : [[h - w, 0], [0, 0], [0, 0]]
+  img = img.pad(pad)
+  const size = APP.size
+  img = tf.image.resizeBilinear(img, [size, size]).reshape([1, size, size, 3])
+  const offset = tf.scalar(127.5)
+  return img.sub(offset).div(offset)
+}
+
+function draw(img, size) {
+  const scaleby = size[0] / img.shape[0]
+  tf.browser.toPixels(img, APP.canvas)
+  APP.canvas.classList.remove('d-none')
+  APP.canvas.classList.add('d-block')
+  APP.status.classList.add('d-none')
+  setTimeout(() => scaleCanvas(scaleby), 50)
+}
+
+function scaleCanvas(pct=2) {
+  const canvas = APP.$('result')
+  const tmpcan = document.createElement('canvas')
+  const tctx = tmpcan.getContext('2d')
+  const cw = canvas.width
+  const ch = canvas.height
+  tmpcan.width = cw
+  tmpcan.height = ch
+  tctx.drawImage(canvas, 0, 0)
+  canvas.width *= pct
+  canvas.height *= pct
+  const ctx = canvas.getContext('2d')
+  ctx.drawImage(tmpcan, 0, 0, cw, ch, 0, 0, cw*pct, ch*pct)
+  APP.download.href = canvas.toDataURL('image/jpeg')
+}
+
+document.getElementById('file').addEventListener('change', evt => {
+  evt.target.files.forEach(f => {
+    if (!f.type.match('image.*')) { return }
+    let reader = new FileReader()
+    reader.onload = e => { APP.source.src = e.target.result }
+    reader.readAsDataURL(f)
+  })
+  evt.target.value = null
+})
+
+document.querySelectorAll('#examples img').forEach(
+  img => img.addEventListener('click', evt => { APP.source.src = img.src })
+)
